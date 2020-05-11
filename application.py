b294924fbcebd964700ba089fb9a6b62a5db821f
Christopher Smallwood <christopher.smallwood@citrix.com>
2020-05-11 13:49:18 +0100

    adding keyvault
    Signed-off-by: Christopher Smallwood <christopher.smallwood@citrix.com>

--- application.py
+++ application.py
@@ -1,6 +1,15 @@
 from flask import Flask
+
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url="https://googletomicroappsvault.vault.azure.net/", credential=credential)
+    retrieved_secret = client.get_secret("GoogleClientSecret")
+    print(retrieved_secret.name)
+    print(retrieved_secret.value)
     return "Hello World!"
